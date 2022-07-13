8d5718bff6def8bd8c47d60c21ae33fabbd8ce31
Kunal Nagwanshi <nagwanshikunal23@gmail.com>
2022-07-13 18:23:09 +0530

    Create java again

---
+++ DataTypes.java
@@ -0,0 +1,19 @@
+public class DataTypes
+{
+    public static void main(String[] args){
+        byte a = 127;
+        short b = 32767;
+        int c = 2147483647;
+        long d = (2^63)-1;
+        float e = 23.0001f;
+        double f =  23.00000001;
+        boolean g = true;
+        boolean h = false;
+        long i;
+        i = (2^64);
+
+        System.out.println(" " + a+ " "+ b +" "+ c+" "+ d+" "+e+" "+f+" "+g+" "+h+" "+i);
+
+
+    }
+}
