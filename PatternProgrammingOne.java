68a8b5e727a1d319c2346d6fdcee4fde7344a29f
kunal nagwanshi <nagwanshikunal23@gmail.com>
2022-07-16 13:35:23 +0530

    Merge remote-tracking branch 'origin/main'

--- PatternProgrammingOne.java
+++ PatternProgrammingOne.java
@@ -6,10 +6,12 @@ public class PatternProgrammingOne
        Pattern1 p1 = new Pattern1();
        Pattern1 p2 = new Pattern1();
        Pattern1 p3 = new Pattern1();
+       Pattern1 p4 = new Pattern1();
 
       p2.pattern2();
        p3.pattern3();
         p1.pattern1();
+        p4.pattern4();
 
 
 
@@ -22,8 +24,18 @@ public class PatternProgrammingOne
 }
    class Pattern1 {
 
+    void pattern4(){
+        for(int i=5;i>=1;i--){
+            for(int j=1;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println("*");
+
+        }
+    }
+
        void pattern3(){
-           for (int i = 1; i <= 5; i++) {
+           for (int i = 1; i < 5; i++) {
                for (int j = 1; j < i; j++) {
                    System.out.print("$");
                }
