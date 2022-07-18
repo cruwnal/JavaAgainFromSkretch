9db1e8ea2d9db7334188a401583f6d66728d961c
kunal nagwanshi <nagwanshikunal23@gmail.com>
2022-07-18 14:08:03 +0530

    Merge remote-tracking branch 'origin/master1'

---
+++ OctalNum.java
@@ -0,0 +1,20 @@
+public class OctalNum {
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 045;
+        int c = 0x45;
+        int d = 0b00110010;
+        System.out.println(a + "\n" + "\n" + b + "\n" + c + "\n" + d);
+        assignment();
+    }
+
+    static void assignment() {
+
+        for (int i = 0; i < 3 ; i++)
+        {
+            System.out.print("Kodnest");
+            System.out.print("Technologies");
+        }
+
+    }
+}
