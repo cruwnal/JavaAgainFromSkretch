4136979f913d9438bcfaccd2c87ff962bd889636
Kunal Nagwanshi <nagwanshikunal23@gmail.com>
2022-07-31 18:38:40 +0530

    Update Swaping.java

--- Swaping.java
+++ Swaping.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class Swaping
 {
     public static void main(String [] args){
-       // normalSwapping();
-        //arraySwapping();
-        //copyArray();
+        normalSwapping();
+        arraySwapping();
+        copyArray();
         reverseTheCopyArray();
 
     }
